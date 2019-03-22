5266521242c8b47f0fded6109932ec75da361a50
James Donlan <jacketlizard@live.com>
2019-03-22 00:01:32 -0700

    dual moving rectangles

--- main.lua
+++ main.lua
@@ -2,14 +2,16 @@ function love.load()
   Object = require "classic"
   require "rectangle"
   
-  r1 = Rectangle()
-  r2 = Rectangle()
+  r1 = Rectangle(100, 100, 200, 50)
+  r2 = Rectangle(180, 300, 25, 140)
 end
 
 function love.update(dt)
-  r1.update(r1, dt)
+  r1:update(dt)
+  r2:update(dt)
 end
 
 function love.draw()
-  r1.draw(r1)
+  r1:draw()
+  r2:draw()
 end
--- rectangle.lua
+++ rectangle.lua
@@ -1,18 +1,18 @@
 --Note how it uses a column (:) and not a dot (.)
 Rectangle = Object:extend()
 
-function Rectangle.new(self)
-    self.x = 100
-    self.y = 100
-    self.width = 200
-    self.height = 150
+function Rectangle:new(x, y, width, height)
+    self.x = x
+    self.y = y
+    self.width = width
+    self.height = height
     self.speed = 100
 end
 
-function Rectangle.update(self, dt)
+function Rectangle:update(dt)
   self.x = self.x + self.speed * dt
 end
 
-function Rectangle.draw(self)
+function Rectangle:draw()
   love.graphics.rectangle("line", self.x, self.y, self.width, self.height)
 end
