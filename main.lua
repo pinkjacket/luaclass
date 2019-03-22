3daffc99007a5e4ce842eac2ae853fa2c9e2a84a
James Donlan <jacketlizard@live.com>
2019-03-21 23:34:26 -0700

    moving rectangle generated via class

--- main.lua
+++ main.lua
@@ -1,1 +1,15 @@
 function love.load()
+  Object = require "classic"
+  require "rectangle"
+  
+  r1 = Rectangle()
+  r2 = Rectangle()
+end
+
+function love.update(dt)
+  r1.update(r1, dt)
+end
+
+function love.draw()
+  r1.draw(r1)
+end
---
+++ rectangle.lua
@@ -0,0 +1,18 @@
+--Note how it uses a column (:) and not a dot (.)
+Rectangle = Object:extend()
+
+function Rectangle.new(self)
+    self.x = 100
+    self.y = 100
+    self.width = 200
+    self.height = 150
+    self.speed = 100
+end
+
+function Rectangle.update(self, dt)
+  self.x = self.x + self.speed * dt
+end
+
+function Rectangle.draw(self)
+  love.graphics.rectangle("line", self.x, self.y, self.width, self.height)
+end
